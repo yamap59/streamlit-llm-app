74b71e1e307f386d1abad8543b225bd4ccc377c3
yamap59 <yamap59@gmail.com>
2025-08-26 20:19:15 +0900

    second commit

--- app.py
+++ app.py
@@ -7,7 +7,12 @@ import os
 load_dotenv()
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
-def call_chat_completion_api(system_message, user_message):
+def call_chat_completion_api(user_message, selected_item):
+    if selected_item == "p5.jsの専門家":
+        system_message = "あなたはp5.jsの専門家です。初心者にもわかりやすく、具体的に、丁寧に説明してください。"
+    else:
+        system_message = "あなたは勉強法の専門家です。初心者にもわかりやすく、具体的に、丁寧に説明してください。"
+
     client = OpenAI(api_key=OPENAI_API_KEY)
     completion = client.chat.completions.create(
         model="gpt-4o-mini",
@@ -36,19 +41,11 @@ def main():
     st.divider()
 
     if user_message := st.chat_input("質問してみましょう"):
-        if selected_item == "p5.jsの専門家":
-            st.chat_message("user").markdown(user_message)
-            ai_message = call_chat_completion_api(
-                system_message="あなたはp5.jsの専門家です。初心者にもわかりやすく、具体的に、丁寧に説明してください。",
-                user_message=user_message
-            )
-            st.chat_message("assistant").markdown(ai_message)
-        else:
-            st.chat_message("user").markdown(user_message)
-            ai_message = call_chat_completion_api(
-                system_message="あなたは勉強法の専門家です。初心者にもわかりやすく、具体的に、丁寧に説明してください。",
-                user_message=user_message
-            )
-            st.chat_message("assistant").markdown(ai_message)
+        st.chat_message("user").markdown(user_message)
+        ai_message = call_chat_completion_api(
+            user_message=user_message,
+            selected_item=selected_item
+        )
+        st.chat_message("assistant").markdown(ai_message)
 
 main()
